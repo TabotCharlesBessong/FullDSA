30cd57acda3b6056e5dd98ee92122c442ac670bc
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-04-16 22:21:25 +0100

    getting length of a linked list

--- SignlyLinkedList.java
+++ SignlyLinkedList.java
@@ -14,6 +14,10 @@ public class SignlyLinkedList {
 
   public void display() {
     ListNode current = head;
+    if(current == null){
+      System.out.print("null");
+      System.err.println();
+    }
     while(current != null){
       System.out.println(current.data + " --->");
       current = current.next;
@@ -26,12 +30,25 @@ public class SignlyLinkedList {
     head = newNode;
   }
 
+  public int length() {
+    if(head == null) return 0;
+    int count = 0;
+    ListNode current = head;
+    while(current != null){
+      count++;
+      current = current.next;
+    }
+    return count;
+  }
+
   public static void main(String[] args) {
     SignlyLinkedList sll1 = new SignlyLinkedList();
-    sll1.insertFirst(1);
-    sll1.insertFirst(2);
-    sll1.insertFirst(3);
     sll1.insertFirst(4);
+    sll1.insertFirst(3);
+    sll1.insertFirst(2);
+    sll1.insertFirst(1);
+    int len = sll1.length();
+    System.out.println(len);
     sll1.display();
   }
 }
