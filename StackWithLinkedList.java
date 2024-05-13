a43ce381bcd731ecc84e2e5ea3aa1628b9bc12a5
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-05-13 12:27:13 +0100

    stack with linked list

--- StackWithLinkedList.java
+++ StackWithLinkedList.java
@@ -1,3 +1,5 @@
+import java.util.EmptyStackException;
+
 public class StackWithLinkedList {
   private ListNode top;
   private int length;
@@ -16,4 +18,49 @@ public class StackWithLinkedList {
     top = null;
     length = 0;
   }
+
+  public int length(){
+    return length;
+  }
+
+  public boolean isEmpty() {
+    return length == 0;
+  }
+
+  public void push(int data){
+    ListNode temp = new ListNode(data);
+    temp.next = top;
+    top = temp;
+    length++;
+  }
+
+  public int pop(){
+    if(isEmpty()) throw new EmptyStackException();
+    int result = top.data;
+    top = top.next;
+    length--;
+    return result;
+  }
+
+  public int peek() {
+      if (isEmpty()) {
+         throw new EmptyStackException();
+      }
+      return top.data;
+   }
+
+   public static void main(String[] args) {
+      StackWithLinkedList stack = new StackWithLinkedList();
+      stack.push(10);
+      stack.push(15);
+      stack.push(20);
+
+      System.out.println(stack.peek());
+      stack.pop();
+      System.out.println(stack.peek());
+      stack.push(30);
+      stack.pop();
+      stack.push(12);
+      System.out.println(stack.peek());
+   }
 }
